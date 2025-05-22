5cc65b02674aac69a519fe6a849378a805072f42
Dylan Murray <38342216+Dman603@users.noreply.github.com>
2025-05-22 05:44:29 -0700

    Update PE69.py

--- PE69.py
+++ PE69.py
@@ -1,3 +1,5 @@
+### Problem statement at this link: https://projecteuler.net/problem=69
+
 ### The answer is the highest number under a million that you get from multiplying consecutive primes: 2x3x5x7x11x13x17 = 510510
 ### this is because any multiples of these numbers are not relatively prime, so for 2 reasons these will minimize phi(n). The first
 ### is that these primes are the lowest numbers possible, so it lets you multiply the most together. The second reason is that

30ab53ed39a92d08a3584f8bc972798af42e9f3f
Dylan Murray <38342216+Dman603@users.noreply.github.com>
2025-05-22 05:42:38 -0700

    Update PE66.py

--- PE66.py
+++ PE66.py
@@ -4,16 +4,10 @@ from matplotlib import pyplot as plt
 import math
 from decimal import *
 
-### This was the hardest problem yet, and I think it will be the hardest for me for a while. This code is really messy, but I don't really care. I don't understand
-### the decimal package and dont need to, so I just threw in Decimal() around every number for every calculation. Without knowing researching Diophantes equations or 
-### Pell's equation, this would have been impossible, other than by testing things out (and the 2 previous problems were all about continued fractions). However,
-### While I was entering in numbers on the calculator in google, one of the searches I found had the number I entered into the searchbar, and the link was a list of
-### terms in the continued fraction for sqrt(244), which is also 2sqrt(61). The number I was doing calculations with was the result of my brute force function when n=61, 
-### so I realized there must be some relationship between continuted fractions and finding the solution to the diophantes equation in question. However, what did not occur to me
-### was that this relationship was the best way to go about it, as I was almost sure I could limit my brute force search enough to make the problem feasible. However, after
-### realizing that the numbers were so huge that I would need to use extra precision, and that even with the 1000x increase in speed that I had given the algorithm, the 
-### needed precision just took too much power to compute, so I looked online for a hint about the problem. The first sentence of the first result I clicked on mentioned 
-### continuing fractions, and with that information, the pieces of the puzzle fit together in my head, and I started coding a new program.
+### Problem statement at this link: https://projecteuler.net/problem=66
+
+### This was the hardest problem yet, and I think it will be the hardest for me for a while. Without knowing researching Diophantes equations or 
+### Pell's equation, this would have been impossible, other than by testing things out (and the 2 previous problems were all about continued fractions).
 
 
 getcontext().prec = 200

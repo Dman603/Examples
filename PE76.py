552f634fe07f3d6340b288baead5da156de3282a
Dylan Murray <38342216+Dman603@users.noreply.github.com>
2025-05-22 05:53:32 -0700

    Update PE76.py

--- PE76.py
+++ PE76.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-
+### Problem statement at this link: https://projecteuler.net/problem=76
 
 def counting_summations():
     arr = np.zeros([100,100])

ecfb9f0c20220dec8a47ac01b2dea812956ec2b6
Dylan Murray <38342216+Dman603@users.noreply.github.com>
2025-05-22 05:39:24 -0700

    Update PE63.py

--- PE63.py
+++ PE63.py
@@ -1,7 +1,6 @@
-### Solved on the toilet with desmos: ceil(log(x)) = log_b(x) and just count the lines. There is a better way to do it by measuring the fator between 1 and log(b) and seeing
-### how many times you need to multiply that to get 1
-
 import math
+
+### Problem statement at this link: https://projecteuler.net/problem=63
 
 def powerful_digit_counts():
     return sum([math.floor(1/(1-math.log10(i))) for i in range(1,10)])
